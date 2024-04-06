94863037168976c55c725a9232e9a18f91080f72
kdmin0706 <gxxk76@gmail.com>
2024-04-06 18:35:47 +0900

    [feat]: ProductType 체크 메서드 생성   - BOTTLE, BAKERY 인 경우 TRUE 반환

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductType.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductType.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.spring.domain.product;
 
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +12,9 @@ public enum ProductType {
   BAKERY("베이커리");
 
   private final String text;
+
+  public static boolean containsStockType(ProductType productType) {
+    return List.of(BOTTLE, BAKERY).contains(productType);
+  }
+
 }
