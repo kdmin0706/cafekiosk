90fc5038128dbb7e54287004a826658f4c899871
kdmin0706 <gxxk76@gmail.com>
2024-04-23 11:02:20 +0900

    [test]: 테스트 코드 작성   - 통합테스트환경 구축     1. IntegrationTestSupport 생성        - service, repository 레이어 통합 클래스 생성        - @DataJpaTest -> @SpringBootTest     2. ControllerTestSupport 생성        - controller 레이어 통합 클래스 생성

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderStaticsServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderStaticsServiceTest.java
@@ -13,9 +13,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.IntegrationTestSupport;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
 import sample.cafekiosk.spring.domain.order.Order;
@@ -26,8 +24,7 @@ import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
-@SpringBootTest
-class OrderStaticsServiceTest {
+class OrderStaticsServiceTest extends IntegrationTestSupport {
 
   @Autowired
   private OrderStaticsService orderStaticsService;
@@ -43,9 +40,6 @@ class OrderStaticsServiceTest {
 
   @Autowired
   private MailSendHistoryRepository mailSendHistoryRepository;
-
-  @MockBean
-  private MailSendClient mailSendClient;
 
   @AfterEach
   void tearDown() {
